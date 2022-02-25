50c20930dd46fc75caa3af587e354bccbee8d7dd
paulvarkey758 <paulkv1997@gmail.com>
2022-02-25 23:14:33 +0530

    iconic website completed

---
+++ script.js
@@ -0,0 +1,34 @@
+var loader=document.getElementById("preloader");
+window.addEventListener("load",function(){
+    loader.style.display="none";
+});
+
+$(document).ready(function(){
+    $(".counter").counterUp({
+        delay:10,
+        time:1100
+    });
+    $(".menu-icon").click(function(){
+        $(".navlist").toggleClass("navshow");
+    });
+    $(".navlink").click(function(){
+        $(".navlist").removeClass("navshow")
+    })
+});
+
+window.addEventListener("scroll",testimonialAnim);
+
+function testimonialAnim(){
+    var cl=[".testimonial-card-one",".testimonial-card-two",".testimonial-card-three"];
+    for(var i=0;i<3;i++){
+        var content=document.querySelector(cl[i]);
+        var contentPosition=content.getBoundingClientRect().top;
+        var screenPosition=window.innerHeight;
+        if(contentPosition<screenPosition){
+            content.classList.add("testimonial-card-anim");
+        }
+        else{
+            content.classList.remove("testimonial-card-anim");
+        }
+    }
+}
